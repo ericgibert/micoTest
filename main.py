8717b241dee1ba4a9d33335bbb19527c90f772b5
ericgibert@yahoo.fr <ericgibert@yahoo.fr>
2023-01-10 23:36:27 +0800

    4 buttons on screen

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # testing internet connection
 # looking for different SSID thru a hidden file
-import network
+from micropython import const
 from machine import Pin, Timer, I2C
 import time
+import network
 from ssids import SSIDs
 from ssd1306_helper import Display
+from dht import DHT11
 
 onboard_led = Pin("LED", Pin.OUT)
 
@@ -29,16 +31,111 @@ def connect_to_WIFI():
 
 from time import sleep, localtime
 dis = Display(0, 17, 16)
+
+now = localtime()
+
+wlan = connect_to_WIFI()
+if wlan:
+    print("Connected:", wlan.ifconfig())
+else:
+    print ("Trying to connect")
+
+
+# buzzer and DHT11
+sensor = DHT11(Pin(15))
+buzzer = Pin(12, Pin.OUT)
+
+# the 4 buttons around the screen
+NB_BUTTONS = const(4)
+buttons = [Pin(i, Pin.IN, Pin.PULL_DOWN) for i in range(NB_BUTTONS)]
+lastValues = [-1] * NB_BUTTONS
+
+currentState = 99   # to display HOME screen
+lastState = -1
+
+def allReleased():
+    """
+        Wait that all the buttons are realsed - biounce effect
+    """
+    while sum([b.value() for b in buttons]):
+        sleep(0.05)
+
 while True:
-    now = localtime()
-    dis.multiLines(f"""{now[0]}-{now[1]:02}-{now[2]:02}
-{now[3]}:{now[4]:02}:{now[5]:02}""")
+    sleep(0.1)
+    for i, button in enumerate(buttons):
+        if button.value() != lastValues[i]:
+            lastValues[i] = button.value()
+            print(f"B{i} = {lastValues[i]}")
     
-    wlan = connect_to_WIFI()
-    if wlan:
-        print("Connected:", wlan.ifconfig())
-    else:
-        print ("Trying to connect")
+    if lastState != currentState:
+        print("state change:", lastState, "-->", currentState)
+        lastState = currentState
     
+    if currentState == 0:
+        # default waiting state to wait for button to be pressed
+        if lastValues[0]:
+            firstTime = True
+            lastState, currentState = 0, 1
+        elif lastValues[1]:
+            firstTime = True
+            lastState, currentState = 0, 2
+        elif lastValues[2]:
+            firstTime = True
+            lastState, currentState = 0, 3
+        elif lastValues[3]:
+            firstTime = True
+            lastState, currentState = 0, 4
+            
+        # wait for button to be released
+        allReleased()
     
-sleep(1)
+    elif currentState == 1:
+        if firstTime:
+            dis.multiLines("Press any button")
+            firstTime = False
+        if sum(lastValues) > 0:
+            # a button is pressed: let's go back to main screen
+            lastState, currentState = 1, 99        
+    
+    elif currentState == 2:
+        if firstTime:
+            dis.screen("Press HOME", button4="HOME")
+            firstTime = False
+        if lastValues[3]:
+            # button4: let's go back to main screen
+            lastState, currentState = 2, 99  
+    
+    elif currentState == 3:
+        if firstTime:
+            dis.screen("Press STOP", button3="STOP")
+            buzzer.on()
+            firstTime = False
+        if lastValues[2]:
+            # button3: let's go back to main screen
+            buzzer.off()
+            lastState, currentState = 3, 99 
+   
+    elif currentState == 4:
+        if firstTime:
+            now = localtime()
+            sensor.measure()
+            temperature = sensor.temperature()
+            humidity=sensor.humidity()
+            dis.screen(f"""{now[3]}:{now[4]:02}:{now[5]:02}
+Temp: {temperature}C
+Humidity: {humidity}%""", button4="Home")
+            firstTime = False
+        if lastValues[3]:
+            # button4: let's go back to main screen
+            lastState, currentState = 2, 99  
+   
+    elif currentState == 99:
+        # display the HOME screen and go to waiting a press (state = 0)
+        dis.screen(f"""{now[0]}-{now[1]:02}-{now[2]:02}
+{now[3]}:{now[4]:02}:{now[5]:02}
+Connected to
+{wlan.config("ssid")}""",
+button1="1", button2="2",button3="3", button4="4")
+        lastState, currentState = 99, 0
+        allReleased()
+        
--- ssd1306_helper.py
+++ ssd1306_helper.py
@@ -1,16 +1,23 @@
-# small python module to help manging thessd1306 OLED display
+"""
+  Small python module to help manging the ssd1306 OLED display
+  - multiLines: display a multi-lines text separated by \n
+  - screen: display a full screen with 4 buttons, title, footer and text
+  - test: display text for testing the display capacity
+  
+"""
 from micropython import const
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 
 WIDTH=const(128)
 HEIGHT=const(64)
-FONTSIZE=const(10)  #  code to improve when we can change the font size
+FONTSIZE=const(10)  #  code to improve when we can change the font size (8px by 10px)
 
-class Display:
+class Display(SSD1306_I2C):
     def __init__(self, port, scl, sda, width=WIDTH, height=HEIGHT, freq=400000):
         i2c = I2C(port, scl=Pin(scl), sda=Pin(sda), freq=freq)
-        self.display = SSD1306_I2C(width, height, i2c)
+        self.fontSize = FONTSIZE  # default font size
+        super().__init__(width, height, i2c)
 
     def multiLines(self, lines, topMargin=0, leftMargin=0):
         """
@@ -20,18 +27,57 @@ class Display:
         :param leftMargin: left margin in pixel
         :return:
         """
-        self.display.fill(0)  # erase current screnn to black
-        x,y = leftMargin, topMargin
+        self.fill(0)  # erase current screnn to black
+        x, y = leftMargin, topMargin
         for line in lines.split('\n'):
-            self.display.text(line, x, y)
-            y += FONTSIZE
-        self.display.show()
+            self.text(line, x, y)
+            y += self.fontSize
+        self.show()
+
+
+    def screen(self, mLines, title="", footer="", button1="", button2="", button3="", button4=""):
+        """
+            Display the following text:
+            
+            1    Title   2
+            mLine 1.......
+            mLine 2.......
+            mLine 3.......
+            mLine 4.......
+            3    Footer  4
+        
+        This code is currently written 
+        """
+        charByLine = WIDTH // 8  #  font 10 -->  8px by 10px
+        nbLines = HEIGHT // self.fontSize - 2  # becasue 1st line is for Title and last line for Footer
+        t, b1, b2 = f"{title.strip():^{charByLine}}", button1.strip(), button2.strip()
+        titleLine = b1 + t[len(b1):-len(b2)] + b2
+        f, b3, b4 = f"{footer.strip():^{charByLine}}", button3.strip(), button4.strip()
+        footerLine = b3 + f[len(b3):-len(b4)] + b4
+        mLines += "\n\n\n\n"
+        mLines = '\n'.join([ l[:charByLine].strip() for l in (mLines.split('\n'))[:4] ])
+        self.multiLines(f"""{titleLine}
+{mLines}
+{footerLine}""", leftMargin=1, topMargin=2)
+
+    def test(self):
+        """
+        just to display a full text for counting characters
+        with font 10px: 15char x 6.2 lines
+        """
+        s = "0123456789"
+        l = s * 2
+        t = "\n".join([l for i in range(7)])
+        self.multiLines(t)
 
 # if __name__ == "__main__":
 #     from time import sleep, localtime
 #     dis = Display(0, 17, 16)
+#     dis.screen("\nline 1 qui est tres longue\net ligne 2\nplus courte",
+#                title="Title", footer="Footer",
+#                button1="1", button2="2",button3="3", button4="4")
 #     while True:
 #         now = localtime()
 #         dis.multiLines(f"""{now[0]}-{now[1]:02}-{now[2]:02}
-#     {now[3]}:{now[4]:02}:{now[5]:02}""")
+# {now[3]}:{now[4]:02}:{now[5]:02}""")
 #     sleep(1)
